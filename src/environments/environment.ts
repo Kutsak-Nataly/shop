7b6bde31d73b450156f7ce391e377f09f61506d2
Kutsak-Nataly <73638937+Kutsak-Nataly@users.noreply.github.com>
2021-02-07 19:58:21 +0300

    Reactive Forms. Guards. Токен

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyB9j_0JXXx0cilpGqh6b95k9eMYxaisw78",
 };
 
 /*
